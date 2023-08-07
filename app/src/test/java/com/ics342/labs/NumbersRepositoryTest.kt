98d93b35c090efa7929ca598b81576a2fdeb0000
JoshStout <JoshStout>
2023-08-06 20:41:50 -0500

    lab9

--- app/src/test/java/com/ics342/labs/NumbersRepositoryTest.kt
+++ app/src/test/java/com/ics342/labs/NumbersRepositoryTest.kt
@@ -1,10 +1,13 @@
 package com.ics342.labs
 
+import io.mockk.Runs
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
 import java.util.UUID
 import kotlin.random.Random
 
@@ -35,6 +38,26 @@ internal class NumbersRepositoryTest {
 
     @Test
     fun ifDatabaseIsEmptyShouldFetchNumbersFromApi() {
-        // TODO: implement this test
+        // implement this test
+
+        // Setup
+        val database = mockk<Database>()
+        val api = mockk<Api>()
+        val number = Number(UUID.randomUUID().toString(), Random.nextInt())
+
+        every { database.getAllNumbers() } returns listOf()
+        every { api.getNumbers() } returns listOf(number)
+        every { database.storeNumbers(listOf(number)) } just Runs
+
+        // Act
+        val numbersRepository = NumbersRepository(database, api)
+        val result = numbersRepository.fetchNumbers()
+
+        // Assert
+        assertEquals(result, listOf(number))
+
+        verify { database.getAllNumbers() }
+        verify { api.getNumbers() }
+
     }
 }
